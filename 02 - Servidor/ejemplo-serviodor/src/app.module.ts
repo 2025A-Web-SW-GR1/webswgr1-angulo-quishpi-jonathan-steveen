89694a4b6089d24f00285910f0d7979295cb32b5
JonathanA203 <jonathan.angulo@epn.edu.ec>
2025-06-24 17:36:31 -0500

    Added to use ORM and sqlite

--- 02 - Servidor/ejemplo-serviodor/src/app.module.ts
+++ 02 - Servidor/ejemplo-serviodor/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { Casa } from './casa/casa.entity';
+import { CasaModule } from './casa/casa.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule, // Importa el módulo de la base de datos
+    CasaModule, // Importa el módulo de Casa
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
